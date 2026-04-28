edad8dfd756300f60aa92bf93bfa37c7726cb0e1
zingrx <mnitpro@gmail.com>
2026-04-28 22:56:32 +0300

    [TMP] fuxi: Add LunarisDolby app

--- device.mk
+++ device.mk
@@ -24,9 +24,12 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 $(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/config/permissions/camerax-vendor-extensions.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/camerax-vendor-extensions.xml
+
+# Dolby by Lunaris
+PRODUCT_PACKAGES += \
+    LunarisDolby
 # Init
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/init.fuxi.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.fuxi.rc \
